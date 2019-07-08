bbcba22ed3e36c405845c64dfbefc50f5c1d4b7d
zhangtj <1771988873@qq.com>
2019-07-08 23:10:04 +0800

    init settingDrawer

---
+++ src/components/SettingDrawer/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <!--<a-button type="primary" @click="showDrawer">
+						Open
+				</a-button>-->
+    <a-drawer
+      title="Basic Drawer"
+      placement="right"
+      :closable="false"
+      @close="onClose"
+      :visible="visible"
+      width="300"
+    >
+      <template v-slot:handle>
+        <div class="handle" @click="visible = !visible">
+          <a-icon :type="visible ? 'close' : 'setting'"></a-icon>
+        </div>
+      </template>
+      <p>Some contents...</p>
+      <p>Some contents...</p>
+      <p>Some contents...</p>
+    </a-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index",
+  data() {
+    return {
+      visible: false
+    };
+  },
+  methods: {
+    /*showDrawer() {
+                this.visible = true
+            },*/
+    onClose() {
+      this.visible = false;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.handle {
+  position: absolute;
+  top: 240px;
+  right: 300px;
+  width: 48px;
+  height: 48px;
+  background: #1890ff;
+  text-align: center;
+  font-size: 20px;
+  color: #fff;
+  line-height: 48px;
+  border-radius: 3px 0 0 3px;
+}
+</style>
--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -1,59 +1,66 @@
 <template>
-<a-layout id="components-layout-demo-side" style="min-height: 100vh">
-		<a-layout-sider :trigger="null" collapsible v-model="collapsed" >
-				<div class="logo" />
-				<SiderMenu />
-		</a-layout-sider>
-		<a-layout>
-				<a-layout-header style="background: #fff; padding: 0" >
-						<a-icon  class="tigger" :type="collapsed ? 'menu-unfold' : 'menu-fold'" @click="collapsed    =    ! collapsed"></a-icon>
-						<Header />
-				</a-layout-header>
-				<a-layout-content style="margin: 0 16px">
-						<router-view />
-				</a-layout-content>
-				<a-layout-footer style="text-align: center">
-						<Footer />
-				</a-layout-footer>
-		</a-layout>
-</a-layout>
-  <!--<div>
+  <div>
+    <a-layout id="components-layout-demo-side" style="min-height: 100vh">
+      <a-layout-sider :trigger="null" collapsible v-model="collapsed">
+        <div class="logo" />
+        <SiderMenu />
+      </a-layout-sider>
+      <a-layout>
+        <a-layout-header style="background: #fff; padding: 0">
+          <a-icon
+            class="tigger"
+            :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+            @click="collapsed = !collapsed"
+          ></a-icon>
+          <Header />
+        </a-layout-header>
+        <a-layout-content style="margin: 0 16px">
+          <router-view />
+        </a-layout-content>
+        <a-layout-footer style="text-align: center">
+          <Footer />
+        </a-layout-footer>
+      </a-layout>
+    </a-layout>
+    <settingDrawer />
+    <!--<div>
 			<Header />
 			<SiderMenu />
 			<router-view />
 			<Footer />
 	</div>-->
+  </div>
 </template>
 
 <script>
-
-import Header from './Header';
+import Header from "./Header";
 import Footer from "./Footer";
 import SiderMenu from "./SiderMenu";
+import SettingDrawer from "../components/SettingDrawer";
 
 export default {
-    name: "BasicLayout",
-    data() {
-        return {
-            collapsed: false,
-        }
-    },
-		components: {
-        Header,
-				Footer,
-				SiderMenu
-		}
-}
-
+  name: "BasicLayout",
+  data() {
+    return {
+      collapsed: false
+    };
+  },
+  components: {
+    Header,
+    Footer,
+    SiderMenu,
+    SettingDrawer
+  }
+};
 </script>
 
 <style scoped>
 .tigger {
-		padding: 0 20px;
-		line-height: 64px;
-		font-size: 20px;
+  padding: 0 20px;
+  line-height: 64px;
+  font-size: 20px;
 }
 .tigge:hover {
-		background-color: #eeeeee;
+  background-color: #eeeeee;
 }
 </style>
