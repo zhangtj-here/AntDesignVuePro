1b08af3c32eaeb2a7ef84f950e82a9b09db718ec
zhangtj <1771988873@qq.com>
2020-04-17 17:19:55 +0800

    创建自动校验，功能点： 初始值的赋值，动态改变值，表单的收集及校验

--- src/views/Forms/BasicForm.vue
+++ src/views/Forms/BasicForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-form :layout="formLayout">
+  <a-form :layout="formLayout" :form="form">
     <a-form-item
       label="Form Layout"
       :label-col="formItemLayout.labelCol"
@@ -20,22 +20,40 @@
         </a-radio-button>
       </a-radio-group>
     </a-form-item>
+    <!-- :validateStatus="fieldAStatus"
+    :help="fieldAHelp" -->
     <a-form-item
       label="Field A"
       :label-col="formItemLayout.labelCol"
       :wrapper-col="formItemLayout.wrapperCol"
-      :validateStatus="fieldAStatus"
       has-feedback
-      :help="fieldAHelp"
     >
-      <a-input allow-clear v-model="fieldA" placeholder="input placeholder" />
+      <a-input
+        v-decorator="[
+          'fieldA',
+          {
+            initialValue: fieldA,
+            rules: [
+              {
+                required: true,
+                min: 6,
+                message: '必须大于5个字符'
+              }
+            ]
+          }
+        ]"
+        allow-clear
+        placeholder="input placeholder"
+      />
+      <!-- v-model="fieldA"  -->
     </a-form-item>
     <a-form-item
       label="Field B"
       :label-col="formItemLayout.labelCol"
       :wrapper-col="formItemLayout.wrapperCol"
     >
-      <a-input v-model="fieldB" placeholder="input placeholder" />
+      <a-input v-decorator="['fieldB']" placeholder="input placeholder" />
+      <!-- v-model="fieldB" -->
     </a-form-item>
     <a-form-item :wrapper-col="buttonItemLayout.wrapperCol">
       <a-button type="primary" @click="handleSubmit">
@@ -48,33 +66,39 @@
 <script>
 export default {
   data() {
+    this.form = this.$form.createForm(this); // 创建表单模型
     return {
       formLayout: "horizontal",
-      fieldA: "",
-      fieldB: "",
-      fieldAStatus: "",
-      fieldAHelp: "",
-      labelCol: {
-        xs: { span: 24 },
-        sm: { span: 5 }
-      },
-      wrapperCol: {
-        xs: { span: 24 },
-        sm: { span: 12 }
-      }
+      fieldA: "hello",
+      fieldB: ""
+      // fieldAStatus: "",
+      // fieldAHelp: "",
+      // labelCol: {
+      //   xs: { span: 24 },
+      //   sm: { span: 5 }
+      // },
+      // wrapperCol: {
+      //   xs: { span: 24 },
+      //   sm: { span: 12 }
+      // }
     };
   },
-  watch: {
-    fieldA(val) {
-      if (val.length <= 5) {
-        this.fieldAStatus = "error";
-        this.fieldAHelp = "必须大于5个字符";
-      } else {
-        this.fieldAStatus = "";
-        this.fieldAHelp = "";
-      }
-    }
+  mounted() {
+    setTimeout(() => {
+      this.form.setFieldsValue({ fieldA: "hello world" }); // 更改表单模型里面的值
+    }, 3000);
   },
+  // watch: {
+  //   fieldA(val) {
+  //     if (val.length <= 5) {
+  //       this.fieldAStatus = "error";
+  //       this.fieldAHelp = "必须大于5个字符";
+  //     } else {
+  //       this.fieldAStatus = "";
+  //       this.fieldAHelp = "";
+  //     }
+  //   }
+  // },
   computed: {
     formItemLayout() {
       const { formLayout } = this;
@@ -99,15 +123,24 @@ export default {
       this.formLayout = e.target.value;
     },
     handleSubmit() {
-      if (this.fieldA.length <= 5) {
-        this.fieldAStatus = "error";
-        this.fieldAHelp = "必须大于5个字符";
-      } else {
-        console.log({
-          fieldA: this.fieldA,
-          fieldB: this.fieldB
-        });
-      }
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log(values);
+          // this.fieldA = values.fieldA;
+          Object.assign(this, values); // 更新数据data里的值
+        } else {
+          console.log(err);
+        }
+      });
+      //   if (this.fieldA.length <= 5) {
+      //     this.fieldAStatus = "error";
+      //     this.fieldAHelp = "必须大于5个字符";
+      //   } else {
+      //     console.log({
+      //       fieldA: this.fieldA,
+      //       fieldB: this.fieldB
+      //     });
+      //   }
     }
   }
 };
